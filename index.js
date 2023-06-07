26cf7e17b2e97aa65bbee7a4be59f43ade676ac0
Ian Hrydziuszko <132368002+robotsWhoSmokeCrack@users.noreply.github.com>
2023-06-07 13:44:30 -0400

    Add all functionality js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function getFormData(){
         book.value,
         author.value,
         pages.value,
-        read.value)
+        read.checked)
         console.log(formSubmit)
         myLibrary.push(formSubmit)
         showBooks();
@@ -35,16 +35,18 @@ function Book (book, author, pages, read){
 	this.book = book
 	this.author = author
 	this.pages = pages
-	this.read = read	
+	this.read = read
+    this.card = `card-${Math.floor(Math.random()*3)+1}`
 }
 
 function showBooks (){
     console.log("Ran showBooks");
-        document.querySelector(".display>div").innerHTML = "";
+        document.querySelector(".console").innerHTML = "";
         myLibrary.forEach((book,i)=>{
         console.log(book.book+ ` interval: ${i}`)
         let card = document.createElement("div");
-        card.classList.add(`card-${Math.floor(Math.random()*3)+1}`);
+        card.classList.add(book.card);
+        card.classList.add("card");             //`card-${Math.floor(Math.random()*3)+1}`);
         card.setAttribute("id",`index-${i}`);
        
         let cardTitle = document.createElement("div");
@@ -59,7 +61,7 @@ ${book.author}`;
         card.appendChild(cardTitle);
         card.appendChild(cardAuthor);
     
-       document.querySelector(".display>div").appendChild(card);})
+       document.querySelector(".console").appendChild(card);})
        
     }
 showBooks();
@@ -73,32 +75,108 @@ function expandBook (){
 let submitBook = document.querySelector("form button");
     submitBook.addEventListener("click",getFormData);
 
+let readCheck = document.querySelector("#update-read");
+
+readCheck.addEventListener("click",function(){console.log("checkbox clicked")
+    myLibrary[bookSelected.substring(6)].read = readCheck.checked;
+    saveBooks();
+})
+
 const wrapper = document.querySelector(".console");
 let bookSelected;
 
 wrapper.addEventListener('click', (event) => {
-   if (event.target.id.substring(0,5) == "index"){
-    bookSelected = event.target.id
-    showSelectedBook(bookSelected);
-    }
+    // console.log(event.currentTarget);
+    console.log(event.target);
+    console.log(`parentNode is ${event.target.parentNode.classList}`)
+    let thisNode = event.target;
+    let thisParent = event.target.parentNode
+ 
+if(thisNode.classList == "console"){
+        return;}
+if(document.querySelector(".selected-book") !== null){console.log(document.querySelector(".selected-book"));
+    document.querySelector(".selected-book").remove();
+}; 
 
-  console.log(event.target.id)})
+if(thisNode.classList == "selected-book" || thisParent.classList == "selected-book"){
+    hideThings(updaters);
+    return;}
+// if (event.target.classList.contains("selected-book")){
+//     document.querySelector(".selected-book").remove();
+//     document.querySelector(".sidebar-button").classList.toggle("hidden"); 
+// }   else
+if (thisNode.id.substring(0,5) == "index"){
+    bookSelected = thisNode.id
+    showSelectedBook(bookSelected);
+    console.log(bookSelected)
+} else 
+if ( thisParent.id.substring(0,5) === "index"){
+    bookSelected = thisParent.id
+    showSelectedBook(bookSelected);
+    console.log(bookSelected)
+    } 
+
+
+
+//   console.log(event.target.id)
+})
 
   function showSelectedBook(book){
     let selectedBook = document.createElement("div");
     selectedBook.classList.add("selected-book");
-    selectedBook.textContent = document.getElementById(book).textContent;
+    selectedBook.setAttribute("id",`selected-${book}`);
+    // selectedBook.textContent = document.getElementById(book).textContent;
+    console.log(`bookSelected is ${bookSelected.substring(6)} and book is ${book}`);
+    let title = document.createElement("div");
+    title.classList.add("selected-title");
+    title.textContent = myLibrary[bookSelected.substring(6)].book;
+
+    let author = document.createElement("div");
+    author.classList.add("selected-author");
+    author.textContent = myLibrary[bookSelected.substring(6)].author;
+
+    let pages = document.createElement("div");
+    pages.classList.add("selected-pages");
+    pages.textContent = myLibrary[bookSelected.substring(6)].pages;
+
+    console.log("isRead?" + myLibrary[bookSelected.substring(6)].read);
+    document.querySelector(".sidebar-check>input").checked = myLibrary[bookSelected.substring(6)].read;
+
+    selectedBook.appendChild(title);
+    selectedBook.appendChild(author);
+    selectedBook.appendChild(pages);
+
     document.getElementById(book).prepend(selectedBook);
-    document.querySelector(".sidebar-button").classList.toggle("hidden");
+    // console.log(selectedBook.nextElementSibling);
+    // title.textContent = selectedBook.nextElementSibling.textContent;
+    // author.textContent = selectedBook.nextElementSibling.nextElementSibling.textContent;
+    // document.querySelector(".sidebar-button").classList.toggle("hidden");
+    // document.querySelector(".sidebar-check").classList.toggle("hidden");
+    console.log("selected book is" +document.querySelector(".console .selected-book"));
+    if(document.querySelector(".console .selected-book") !== null){console.log("selected book is" +document.querySelector(".console .selected-book"))
+        showHidden(updaters)
+    } else {
+        hideThings(updaters)
+    }
   }
 
   document.querySelector(".sidebar-button").addEventListener("click",
-    function (){myLibrary.splice(bookSelected.substring(6),1)
+    function removeBook(){myLibrary.splice(bookSelected.substring(6),1)
         document.querySelector(".selected-book").remove();
         document.querySelector(".sidebar-button").classList.toggle("hidden");
         showBooks();
         saveBooks();
     })
+let updaters = [document.querySelector(".sidebar-button"),document.querySelector(".sidebar-check")]
+let form = Array.from(document.querySelectorAll("form>*"));
+
+function showHidden(hiddenThings){
+    hiddenThings.forEach(thing => thing.classList.remove("hidden"))
+}
+
+function hideThings(things){
+    things.forEach(thing => thing.classList.add("hidden"))
+}
 
 
 
